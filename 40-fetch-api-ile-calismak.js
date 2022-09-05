4065948d9d3379f1f6459b58c52076028de4bf13
fatihylcn1 <bjk-fatih-2002@hotmail.com>
2022-09-06 01:08:44 +0300

    30Days Learning Javascript Dosyaları Güncellendi

---
+++ 40-fetch-api-ile-calismak.js
@@ -0,0 +1,23 @@
+// fetch api ile calismak
+
+
+// JSON dosyadan veri cekmek:
+fetch("data/settings.json").then(
+    response => 
+        response.json()
+).then(responseJson => {
+    console.log(responseJson)
+    console.log(responseJson.userName)
+})
+
+let userListDOM = document.querySelector('#userList')
+// API uzerinden veri çektik
+fetch("https://jsonplaceholder.typicode.com/posts").then(
+    response => response.json()
+).then(responseJson => {
+    responseJson.forEach(item => {
+        let liDOM = document.createElement('li')
+        liDOM.innerHTML = item.title
+        userListDOM.appendChild(liDOM)
+    })
+})
